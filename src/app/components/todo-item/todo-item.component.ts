92fd4c53ca5c149ebe2090b07ae0d232730d5767
KamalGaider <kamalgaider@gmail.com>
2019-03-02 18:41:32 +0100

    Added new components

---
+++ src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+  @Input() todoitem: Todo
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+//which css classes to add to html
+setClass(){
+  let cssClass= {
+    todoClass: true,
+    'is-complete': this.todoitem.completed
+  }
+  return cssClass;
+}
+
+toggleTodo( t ){
+this.todoitem.completed = !t.completed;
+}
+
+deleteTodo(){
+
+}
+
+}
